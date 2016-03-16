62d83f2ad06cf2152f0bf1c925e7ac69affc16e9
xhw <835913166@qq.com>
2016-03-16 10:16:39 +0800

    搭建框架

---
+++ MYgitProgect/MYgitProgect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYgitProgect
+//
+//  Created by 夏浩文 on 16/3/16.
+//  Copyright © 2016年 夏浩文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MYgitProgect/MYgitProgect/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MYgitProgect
+//
+//  Created by 夏浩文 on 16/3/16.
+//  Copyright © 2016年 夏浩文. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
